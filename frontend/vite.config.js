e6396138b122c1d74f359dd7bfcf04ff86f595e7
Shivam-kum-mhta <155562554+Shivam-kum-mhta@users.noreply.github.com>
2025-04-08 19:05:24 +0000

    gone through process of building while initiating add_alarm.js file as background.js (service worker)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: 'public/manifest.json',
-        dest: '.',
-      }
-    ],
-  }),
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: '.', // Copy manifest.json to the root of the build directory
+        },
+        {
+          src: 'src/alarms/add_alarm.js',
+          dest: '.', // Copy and rename add_alarm.js to background.js
+          rename: 'background.js',
+        },
+      ],
+    }),
   ],
   build: {
     outDir: 'build',
     rollupOptions: {
       input: {
-        main: './index.html',
+        main: './index.html', // Entry point for the app
       },
     },
   },
-})
+});
